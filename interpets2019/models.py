3a58ca6ef5f822f2be2efc216e30b5d93ca86c58
olivato <olivatooo@gmail.com>
2019-06-07 16:38:46 -0300

    Adicionado telefone como opcional

--- interpets2019/models.py
+++ interpets2019/models.py
@@ -7,7 +7,7 @@ TIPOS = [('Vegetariano', 'Vegetariano'), ('Vegano', 'Vegano'), ('Nenhuma', 'Nenh
 class Petiano(models.Model):
     nome = models.CharField(max_length=255, null=False, default='')
     email = models.CharField(max_length=255, unique=True, default='')
-    telefone = models.CharField(max_length=255, unique=False)
+    telefone = models.CharField(max_length=255, unique=False,null=True)
     restricao_alimentar = models.CharField(max_length=255, unique=False, default='Nenhuma', choices=TIPOS)
     pet = models.CharField(max_length=25, null=False, default='')
     oficina = models.BooleanField(default=False, null=True)
