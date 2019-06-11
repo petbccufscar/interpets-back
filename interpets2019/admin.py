a6701fd800030f043ec49b8b609c0c8605a2c4b4
Cassiano Maia <cassmala@gmail.com>
2019-06-11 18:55:13 -0300

    Adcionando Filtros de campo

--- interpets2019/admin.py
+++ interpets2019/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from interpets2019.models import Petiano
 
-admin.site.register(Petiano)
+# admin.site.register(Petiano)
+@admin.register(Petiano)
+class PetianoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'pet', 'restricao_alimentar', 'oficina')
+    list_filter = ('restricao_alimentar', 'oficina')
