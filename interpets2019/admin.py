dc74175128486411d2b61b3b68daa4572134870f
cassianomaia <cassmala@gmail.com>
2019-06-12 18:40:57 -0300

    Criando filtro para pet

--- interpets2019/admin.py
+++ interpets2019/admin.py
@@ -5,4 +5,4 @@ from interpets2019.models import Petiano
 @admin.register(Petiano)
 class PetianoAdmin(admin.ModelAdmin):
     list_display = ('nome', 'pet', 'restricao_alimentar', 'oficina')
-    list_filter = ('restricao_alimentar', 'oficina')
+    list_filter = ('restricao_alimentar', 'oficina', 'pet')
