9187b1e11fc21eb5b41e8cc802811dbf10bb0417
cassianomaia <cassmala@gmail.com>
2019-06-13 13:35:45 -0300

    Criando função de confirmar pagamento

--- interpets2019/admin.py
+++ interpets2019/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from interpets2019.models import Petiano
 
+
+def confirma_pagamento(modeladmin, request, queryset):
+    for petiano in queryset:
+        petiano.pagou = True
+        petiano.save()
+confirma_pagamento.short_description = 'Confirma Pagamento'
+
 # admin.site.register(Petiano)
 @admin.register(Petiano)
 class PetianoAdmin(admin.ModelAdmin):
     list_display = ('nome', 'pet', 'restricao_alimentar', 'oficina', 'pagou', 'credenciado')
     list_filter = ('restricao_alimentar', 'oficina', 'pet', 'pagou', 'credenciado')
+
