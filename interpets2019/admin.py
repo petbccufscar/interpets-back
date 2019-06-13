86ee0a688d3ead6f3519a0624cdd20d33024620a
cassianomaia <cassmala@gmail.com>
2019-06-13 14:43:50 -0300

    Função de credenciamento

--- interpets2019/admin.py
+++ interpets2019/admin.py
@@ -7,10 +7,16 @@ def confirma_pagamento(modeladmin, request, queryset):
         petiano.save()
 confirma_pagamento.short_description = 'Confirma Pagamento'
 
+def credencia(modeladmin, request, queryset):
+    for petiano in queryset:
+            petiano.credenciado = True
+            petiano.save()
+credencia.short_description = 'Credenciar petiano(s)'
+
 # admin.site.register(Petiano)
 @admin.register(Petiano)
 class PetianoAdmin(admin.ModelAdmin):
     list_display = ('nome', 'pet', 'restricao_alimentar', 'oficina', 'pagou', 'credenciado')
     list_filter = ('restricao_alimentar', 'oficina', 'pet', 'pagou', 'credenciado')
     search_fields = ['nome']
-    actions = [confirma_pagamento, ]
+    actions = [confirma_pagamento, credencia]
