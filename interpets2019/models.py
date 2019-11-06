5fd8905779b122db86b5a65793099dacf69eac08
olivato <olivatooo@gmail.com>
2019-11-06 17:14:15 -0300

    Adicionado GDTs

--- interpets2019/models.py
+++ interpets2019/models.py
@@ -1,37 +1,57 @@
 from django.db import models
 
-TIPOS = [('Vegetariano', 'Vegetariano'), ('Vegano', 'Vegano'), ('Nenhuma', 'Nenhuma')]
+TIPOS = [('Vegetariano', 'Vegetariano'),
+         ('Vegano', 'Vegano'), ('Nenhuma', 'Nenhuma')]
 
+"""
 tipos_gdt = [('Nenhum', 'Nenhum'),
 			('Estrutura Horizontal', 'Estrutura Horizontal'),
 			('Gestão Interna', 'Gestão Interna'),
 			('Desempenho acadêmico', 'Desempenho acadêmico')]
+"""
+
 
 class GDT(models.Model):
-	gdt = models.CharField(max_length=255, default='Nenhum', choices=tipos_gdt)
-	quantidade_vagas = models.CharField(max_length=1, editable=False)
-	petiano = models.ManyToManyField('Petiano', blank=True)
-	
-	class Meta:
-		ordering = ['gdt']
+    nome = models.CharField(max_length=255, default='', null=True, blank=False)
+    quantidade_vagas = models.IntegerField(null=True, blank=False)
 
-	def __str__(self):
-		return str(self.gdt)
+    class Meta:
+        ordering = ['nome']
+
+    def __str__(self):
+        return str(self.nome)
 
 
 class Petiano(models.Model):
-	gdt = models.ForeignKey(GDT, blank = True, on_delete=models.CASCADE)
+    gdt = models.ForeignKey(
+        GDT,
+        blank=True,
+        on_delete=models.CASCADE,
+        related_name='gdt_do_petiano',
+        null=True)
+    nome = models.CharField(max_length=255, null=False, default='')
+    email = models.CharField(max_length=255, unique=True, default='')
+    telefone = models.CharField(max_length=255, unique=False, null=True)
+    restricao_alimentar = models.CharField(
+        max_length=255,
+        unique=False,
+        default='Nenhuma',
+        choices=TIPOS)
+    pet = models.CharField(max_length=255, null=False, default='')
+    credenciado = models.BooleanField(default=False)
+    pagou = models.BooleanField(default=False)
 
-	nome = models.CharField(max_length=255, null=False, default='')
-	email = models.CharField(max_length=255, unique=True, default='')
-	telefone = models.CharField(max_length=255, unique=False, null=True)
-	restricao_alimentar = models.CharField(max_length=255, unique=False, default='Nenhuma', choices=TIPOS)
-	pet = models.CharField(max_length=255, null=False, default='')
-	credenciado = models.BooleanField(default=False)
-	pagou = models.BooleanField(default=False)
+    class Meta:
+        ordering = ['pet']
 
-	class Meta:
-		ordering = ['pet']
+    def __str__(self):
+        return str(self.pet) + ' ' + str(self.nome)
 
-	def __str__(self):
-		return str(self.pet) + ' ' + str(self.nome)
+    def save(self, *args, **kwargs):
+        try:
+            if self.gdt.quantidade_vagas > 0:
+                self.gdt.quantidade_vagas -= 1
+                self.gdt.save()
+        except BaseException:
+            pass
+        super().save(*args, **kwargs)
